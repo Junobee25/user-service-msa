24022202b56402ba9f18b8fa4cafad877edc2ca3
Junobee25 <wnsdhqo@naver.com>
2024-02-05 01:07:09 +0900

    #7 - 로그아웃 구현을 위한 Token 엔티티 및 저장소 생성

---
+++ src/main/java/com/hanghae/userservice/domain/entity/Token.java
@@ -0,0 +1,30 @@
+package com.hanghae.userservice.domain.entity;
+
+import com.hanghae.userservice.domain.constant.TokenType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accessToken;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+
+    @Getter
+    @Setter
+    private Boolean expired;
+
+    @Getter
+    @Setter
+    private Boolean revoked;
+
+    private String email;
+}
