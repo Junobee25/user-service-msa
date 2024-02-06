5486d22023b9978e59e405230bc05f5ef3ef79bb
Junobee25 <wnsdhqo@naver.com>
2024-02-06 12:54:39 +0900

    #7 - apigateway로 요청한 user 인증 처리

--- src/main/java/com/hanghae/userservice/controller/UserAccountController.java
+++ src/main/java/com/hanghae/userservice/controller/UserAccountController.java
@@ -12,18 +12,21 @@ import com.hanghae.userservice.service.UserAccountService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user-service")
 public class UserAccountController {
 
     private final UserAccountService userAccountService;
 
-    @PostMapping("/users/sign-up")
+    @PostMapping("/sign-up")
     public Response<UserSignUpResponse> signUp(UserSignUpRequest request) {
         return Response.success(UserSignUpResponse.from(userAccountService.signUp(
                 request.email(),
@@ -33,29 +36,30 @@ public class UserAccountController {
                 request.profilePicture())));
     }
 
-    @GetMapping("/users/sign-up/confirm-email")
+    @GetMapping("/sign-up/confirm-email")
     public Response<EmailAuthResponse> confirmEmail(@ModelAttribute EmailAuthRequest request) {
         userAccountService.confirmEmail(request.email(), request.authToken());
         return Response.success(new EmailAuthResponse(request.email()));
     }
 
-    @PostMapping("/users/login")
+    @PostMapping("/login")
     public Response<UserLoginResponse> login(UserLoginRequest request) {
         return Response.success(userAccountService.login(request.email(), request.password()));
     }
 
-    @PostMapping("/users/refresh-token")
+    @PostMapping("/refresh-token")
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
         userAccountService.refreshToken(request, response);
     }
 
-    @PutMapping("/users/modify-profile")
+    @PutMapping("/modify-profile")
     public void modifyProfile(UserProfileModifyRequest request, Authentication authentication) {
         //TODO: 프로필 변경시 프로필 사진은 일단 String 으로 대체한다. 후에 변경
+        log.debug(authentication + "**************************************");
         userAccountService.modifyProfile(authentication.getName(), request.userName(), request.memo(), request.profilePicture());
     }
 
-    @PutMapping("/users/modify-password")
+    @PutMapping("/modify-password")
     public Response<UserLoginResponse> modifyPassword(String currentPassword, String newPassword, Authentication authentication) {
         return Response.success(userAccountService.modifyPassword(authentication.getName(), currentPassword, newPassword));
     }
